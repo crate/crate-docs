aed92f0e200015fe2ae5ffa5f5638a9f739fdf7e
Naomi Slater <23469+mechanomi@users.noreply.github.com>
2021-05-26 17:39:17 +0200

    add initial `linkcheck_ignore` list

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,7 @@
 from crate.theme.rtd.conf.doing_docs import *
+
+linkcheck_ignore = [
+    "https://jenkins.crate.io/job/crate.io/job/fastly_purge/",
+    "https://jenkins.crate.io/job/crate_release_and_packaging/job/rebuild_all_docs/",
+    "https://github.com/crate/salt",
+]
